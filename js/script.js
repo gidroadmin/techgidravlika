e23cc23b1328120aed193d6fbaaa070a97ce8d80
gidroadmin <maindmn@gmail.com>
2020-02-05 19:15:12 +0200

    update header

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-    $('.header__burger').click(function(event){
-        $('.header__burger, .left-nav').toggleClass("active");
+
+    $('.header__burger').click(function(event)
+    {
+        $('.header__burger, .left-nav').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+    
+    
+});
+
+$(document).ready(function(){
+    $('.header__logo').click(function(event)
+        {
+            $('.menu').toggleClass('active');
+            $('body').toggleClass('lock');
+        })
+
 });
